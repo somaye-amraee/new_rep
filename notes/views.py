5c49cdac5cb0b9ce1a92ad52863c91151511c4ea
somaye-amraee <82138637+somaye-amraee@users.noreply.github.com>
2021-12-05 15:43:09 +0330

    pagination

---
+++ notes/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, get_object_or_404
+
+from rest_framework.viewsets import ModelViewSet
+from .models import Note
+from .serializers import NoteSerializer
+
+
+class NoteViewSet(ModelViewSet):
+    serializer_class = NoteSerializer
+
+    def get_object(self):
+        return get_object_or_404(Note, id=self.request.query_params.get("id"))
+
+    def get_queryset(self):
+        return Note.objects.filter(is_active=True).order_by('-last_udpated_on')
+
+    def perform_destroy(self, instance):
+        instance.is_active = False
+        instance.save()
---
+++ notesapp/urls.py
@@ -0,0 +1,41 @@
+"""notesapp URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import path
+from notes.views import NoteViewSet
+from django.conf.urls import url
+
+urlpatterns = [
+    # the admin path is present by default when you create the Django profject. It is used to access the Django admin panel.
+    path('admin/', admin.site.urls),
+
+    # the URLs for your APIs start from here
+    url(r'^note$', NoteViewSet.as_view(
+        {
+            'get': 'retrieve',
+            'post': 'create',
+            'put': 'update',
+            'patch': 'partial_update',
+            'delete': 'destroy'
+        }
+    )),
+    url(r'^note/all$', NoteViewSet.as_view(
+        {
+            'get': 'list',
+        }
+    )),
+]
